db4879ba3168ba7a189219107726118b979ec5b9
Tolunay Arslan <tolunayarslan@tolunays-macbook-pro.local>
2018-12-18 15:49:24 +0100

    Initial Commit

---
+++ CS193P-Assignment4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CS193P-Assignment4
+//
+//  Created by Tolunay Arslan on 18.12.18.
+//  Copyright © 2018 Tolunay Arslan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
